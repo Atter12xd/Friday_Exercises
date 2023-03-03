c401e8ca216e60e565a6c8975f46a92d322fd71c
Atter12xd <1485275@senati.pe>
2023-03-03 09:52:22 -0500

    Calculamos_el_Area_y_mostramos

--- Python_ejercicios/ejericios_python.py
+++ Python_ejercicios/ejericios_python.py
@@ -3,3 +3,5 @@
 import math # Importamos la biblioteca math
 print("Ingrese el radio del circulo: ") # ponemos un print para que el usario ponga el radio del circulo 
 radio = float(input()) # usamos float para devolver una representacion de coma flotante de el numero.
+Area = math.pi * (radio * radio) # calculamos el area 
+print("El area del circulo con radio ",radio," Es: ",Area ) # mostramos el area 
