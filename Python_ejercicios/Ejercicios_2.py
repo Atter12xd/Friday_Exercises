0959742dbd54512de169f01fc0f41e6de754058d
Atter12xd <1485275@senati.pe>
2023-03-03 11:51:35 -0500

    tarea_echo_en_clase

---
+++ Python_ejercicios/Ejercicios_2.py
@@ -0,0 +1,9 @@
+# Desarrollar un algoritmo donde permita el ingreso de datos
+
+numeros = [10, 15, 27, 100, 200] 
+lista= list(map(str, numeros))
+print(lista)
+
+numeros = [10, 15, 27, 100, 200]
+tupla = tuple(map(str, numeros))
+print(tupla)
