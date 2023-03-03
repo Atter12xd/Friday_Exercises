50186e63092abf2e346fd16951890ac1953c4cfd
Atter12xd <1485275@senati.pe>
2023-03-03 09:50:46 -0500

    pedimos_al_usario_el_radio_del_circulo

--- Python_ejercicios/ejericios_python.py
+++ Python_ejercicios/ejericios_python.py
@@ -1,3 +1,5 @@
 # Desarrole un algoritmo para hallar el area de un circulo, el usario debe ingresar el radio
 
 import math # Importamos la biblioteca math
+print("Ingrese el radio del circulo: ") # ponemos un print para que el usario ponga el radio del circulo 
+radio = float(input()) # usamos float para devolver una representacion de coma flotante de el numero.
