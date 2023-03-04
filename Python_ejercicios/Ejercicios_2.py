d78096f6a3c5c435a579acb664a4fdd5014b43d0
Atter12xd <1485275@senati.pe>
2023-03-04 07:37:25 -0500

    trabajo_de_clase_actualizado

--- Python_ejercicios/Ejercicios_2.py
+++ Python_ejercicios/Ejercicios_2.py
@@ -1,9 +1,11 @@
 # Desarrollar un algoritmo donde permita el ingreso de datos
 
-numeros = [10, 15, 27, 100, 200] 
-lista= list(map(str, numeros))
-print(lista)
+numeros = input("Ingrese los numeros: ") # Solicitamos numero al usuario
+numeros = [int(numero) for numero in numeros.split(",")] # Convertimos cadena de texto en una lista de numeros
+lista_cadenas = list(map(str, numeros)) # hacemos el proceso para convertir los numeros en "list"
+print("el de la list son", lista_cadenas) # mostramos la lista
 
-numeros = [10, 15, 27, 100, 200]
-tupla = tuple(map(str, numeros))
-print(tupla)
+numeros = input("Ingrese los numeros: ") # Solicitamos numero al usuario
+numeros = [int(numero) for numero in numeros.split(",")]  # Convertimos cadena de texto en una lista de numeros
+lista_cadenas = tuple(map(str, numeros)) # hacemos el proceso para convertir los numeros en "tuple"
+print("el de tuple son: ",lista_cadenas) # mostramos el tuplee
